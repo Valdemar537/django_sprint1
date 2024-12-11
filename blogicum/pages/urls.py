3940bbe4209ad28a1f53e6fa5beaaad18d460a97
Vladimir Pakriev <vladimir537@yandex.ru>
2024-12-11 18:09:38 +0300

    first_commit

---
+++ blogicum/pages/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from pages import views
+
+app_name = 'pages'
+
+urlpatterns = [
+    path('pages/about/', views.about, name='about'),
+    path('pages/rules/', views.rules, name='rules'),
+]
